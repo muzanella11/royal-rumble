cedd657e66a6e38a4669afd9c6dd94e5bd3aa75f
Nurfirliana Muzanella <muzanella11@gmail.com>
2020-03-16 00:01:17 +0800

    Initialize

---
+++ RoyalRumble.java
@@ -0,0 +1,12 @@
+import java.util.List;
+
+public class RoyalRumble {
+  public static void main( String args[] ) throws Exception
+  {
+    System.out.println("Hello world :)");
+  }
+
+  public List<String> getSortedList(List<String> names) {
+    return null;
+  }
+}
